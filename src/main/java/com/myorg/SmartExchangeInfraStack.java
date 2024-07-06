7575ab88233fa5b6fde5d271d78f7ca73607852d
Huseyin Can Ercan <huscanercan@gmail.com>
2024-07-06 23:47:42 +0300

    Initial resources for AWS Lambda

--- src/main/java/com/myorg/SmartExchangeInfraStack.java
+++ src/main/java/com/myorg/SmartExchangeInfraStack.java
@@ -1,5 +1,8 @@
 package com.myorg;
 
+import software.amazon.awscdk.CfnOutput;
+import software.amazon.awscdk.services.lambda.*;
+import software.amazon.awscdk.services.lambda.Runtime;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
@@ -20,5 +23,27 @@ public class SmartExchangeInfraStack extends Stack {
         // final Queue queue = Queue.Builder.create(this, "SmartExchangeInfraQueue")
         //         .visibilityTimeout(Duration.seconds(300))
         //         .build();
+        // Define the Lambda function resource
+        Function myFunction = Function.Builder.create(this, "HelloWorldFunction")
+                .runtime(Runtime.NODEJS_20_X) // Provide any supported Node.js runtime
+                .handler("index.handler")
+                .code(Code.fromInline(
+                        "exports.handler = async function(event) {" +
+                                " return {" +
+                                " statusCode: 200," +
+                                " body: JSON.stringify('Hello World!')" +
+                                " };" +
+                                "};"))
+                .build();
+
+        // Define the Lambda function URL resource
+        FunctionUrl myFunctionUrl = myFunction.addFunctionUrl(FunctionUrlOptions.builder()
+                .authType(FunctionUrlAuthType.NONE)
+                .build());
+
+        // Define a CloudFormation output for your URL
+        CfnOutput.Builder.create(this, "myFunctionUrlOutput")
+                .value(myFunctionUrl.getUrl())
+                .build();
     }
 }
